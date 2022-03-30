b253085cce096d90be3c60fec936bc2e784558b3
EliOrozco <92065409+EliOrozco@users.noreply.github.com>
2022-03-29 19:45:13 -0600

    Combate 2
    Se cambio por las mecánicas de combate de doka pero las cartas no sirven, escribiré más documentación al respecto

--- scr/combate_test.js
+++ scr/combate_test.js
@@ -1,8 +1,5 @@
-import playerW from "./Jugador/jugadorMA.js";
-import NPC from "./NPCMA/NPC.js";
-var follower;
-var path;
-var graphics;
+var card_w, card_a, card_s, card_d;
+var keys;
 export default class combate_test extends Phaser.Scene{
 
 constructor(){
@@ -11,8 +8,9 @@ constructor(){
 
 preload(){
     this.load.image("map", "./assets/combate/mapa_c.png");
-    this.load.image("player", "./assets/combate/player_c.png");
-    this.load.image("pew", "./assets/combate/pew.png");
+    //this.load.image("player", "./assets/combate/player_c.png");
+    this.load.image("card_off", "./assets/combate/card_off.png")
+    this.load.image("card_on", "./assets/combate/card_off.png")
 }
 
 create(){
@@ -20,64 +18,29 @@ create(){
     this.cameras.main.setBounds(0, 0, 500 , 480);
     this.physics.world.setBounds(0, 0, 500, 480);
     this.add.image(0, 0, 'map').setOrigin(0);
-
-    //detalles del sprite
-    this.jugador = new playerW(this, 30, 30, "player");
-    this.jugador.setScale(.1).setOrigin(0,0);
     
-    //proyectil
-    this.pew = new NPC(this, 0, 250, "pew");
-    this.pew.setScale(.2).setOrigin(0,0);
-    graphics = this.add.graphics();
-
-    follower = { t: 0, vec: new Phaser.Math.Vector2() };
-    path = new Phaser.Curves.Path(100, 100);
-
-    path.lineTo(500, 200);
-    path.lineTo(200, 300);
-    path.lineTo(400, 500);
-
-    this.tweens.add({
-        targets: follower,
-        t: 1,
-        ease: 'Sine.easeInOut',
-        duration: 4000,
-        yoyo: true,
-        repeat: -1
-    });
-
-    //fisicas
-    this.jugador.body.setCollideWorldBounds(true);
-    this.physics.add.collider(this.pew, this.jugador);
-    //detalles de la cámara
-    this.cameras.main.startFollow(this.jugador);
-    this.cameras.main.followOffset.set(0, 0);
+    //cartas
+    card_w = this.add.sprite(65, 50, 'card_off').setOrigin(0.5);
+    card_a = this.add.image(15, 90, 'card_off').setOrigin(0.5);
+    card_s = this.add.image(65, 150, 'card_off').setOrigin(0.5);
+    card_d = this.add.image(115, 90, 'card_off').setOrigin(0.5);
+    
     //control
+    keys = this.input.keyboard.addKeys('W,A,S,D');
     this.movimiento = this.input.keyboard.createCursorKeys();
 }
 
 update(time, delta){
-    if (this.movimiento.right.isDown){
-        this.jugador.body.setVelocityX(150);
-    }else if (this.movimiento.left.isDown){
-        this.jugador.body.setVelocityX(-150);
-    }else {
-        this.jugador.body.setVelocityX(0);
-    }
-    
-    if (this.movimiento.up.isDown){
-        this.jugador.body.setVelocityY(-150);
-    }else if (this.movimiento.down.isDown){
-        this.jugador.body.setVelocityY(150);
-    }else {
-        this.jugador.body.setVelocityY(0);  
-    }
-    graphics.clear();
-    path.draw(graphics);
+    /*ESTO NO FUNCIONA*/
+    /*if (this.movimiento.up.isDown){
+        card_w.setTexture('card_on')
+    }else if (this.movimiento.left.isUp){
+        card_w.setTexture('card_off')
+    }*/
 
-    path.getPoint(follower.t, follower.vec);
-
-    graphics.fillStyle(0xff0000, 1);
-    graphics.fillCircle(follower.vec.x, follower.vec.y, 12);
+    card_w.setAlpha((keys.W.isDown) ? 1 : 0.2)
+    card_a.setAlpha((keys.A.isDown) ? 1 : 0.2)
+    card_s.setAlpha((keys.S.isDown) ? 1 : 0.2)
+    card_d.setAlpha((keys.D.isDown) ? 1 : 0.2)
 }
 }
