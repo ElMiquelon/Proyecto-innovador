a797fd57a84f8dddc2ac60b8a4007d212e4052ff
kiskoso <102210617+kiskoso@users.noreply.github.com>
2022-05-11 03:49:53 -0500

    Hitbox edificio C WIP

---
+++ scr/escenas/edificios/edificioCP0.js
@@ -0,0 +1,44 @@
+var jefeFT = true;
+export default class edificioCP0.js extends Phaser.Scene{
+    constructor(){
+        super({key:'edificioCP0.js'});
+    }
+
+    create(){
+ //hitbox del edificio
+ this.hitboxes = this.physics.add.staticGroup([
+    this.add.rectangle(40,0,23,19).setOrigin(0),
+    this.add.rectangle(78,0,20,19).setOrigin(0),
+    this.add.rectangle(64,0,15,10).setOrigin(0),
+    this.add.rectangle(113,0,54,19).setOrigin(0),
+    this.add.rectangle(98,0,15,10).setOrigin(0),
+    this.add.rectangle(182,0,36,19).setOrigin(0),
+    this.add.rectangle(167,0,15,10).setOrigin(0),
+    this.add.rectangle(233,0,38,19).setOrigin(0),
+    this.add.rectangle(218,0,15,10).setOrigin(0),
+    this.add.rectangle(312,0,61,19).setOrigin(0),
+    this.add.rectangle(372,0,16,10).setOrigin(0),
+    this.add.rectangle(388,0,41,19).setOrigin(0),
+    this.add.rectangle(444,0,46,19).setOrigin(0),
+    this.add.rectangle(429,0,15,10).setOrigin(0),
+    this.add.rectangle(490,0,16,10).setOrigin(0),
+    this.add.rectangle(506,0,52,19).setOrigin(0),
+    this.add.rectangle().setOrigin(0),
+
+     //OVERLAP de las escaleras (de izquierda a derecha)
+     this.escalera1 = this.add.zone(271,0,40,19).setOrigin(0);//esta la de en medio que sube
+     
+     
+     this.physics.add.staticGroup([this.escalera1]);
+     this.physics.add.overlap(this.jugador, this.escalera1, ()=>{
+         console.log('subiste por escalera 1 a DP2');
+         this.registry.events.emit('subirescalera1d2');
+         this.input.keyboard.enabled = false;
+         this.scene.transition({target:'edificioDP2', duration:300, sleep:true, moveBelow:true});
+    
+]);
+this.physics.add.collider(this.jugador, this.hitboxes);
+
+
+
+}
---
+++ scr/escenas/edificios/edificioCP1.js
@@ -0,0 +1,24 @@
+export default class edificioCP1.js extends Phaser.Scene{
+    constructor(){
+        super({key:'edificioCP0.js'});
+    }
+
+
+this.add.rectangle(40,0,58,20).setOrigin(0),
+this.add.rectangle(98,0,16,10).setOrigin(0),
+this.add.rectangle(114,0,104,20).setOrigin(0),
+this.add.rectangle(233,19,37,20).setOrigin(0),
+this.add.rectangle(218,0,15,10).setOrigin(0),
+this.add.rectangle(312,0,60,20).setOrigin(0),
+this.add.rectangle(387,0,42,20).setOrigin(0),
+this.add.rectangle(372,0,15,10).setOrigin(0),
+this.add.rectangle(444,0,46,20).setOrigin(0),
+this.add.rectangle(429,0,15,10).setOrigin(0),
+this.add.rectangle(505,0,53,20).setOrigin(0),
+this.add.rectangle(490,0,15,10).setOrigin(0),
+this.add.rectangle().setOrigin(0),
+this.add.rectangle().setOrigin(0),
+this.add.rectangle().setOrigin(0),
+this.add.rectangle().setOrigin(0),
+this.add.rectangle().setOrigin(0),
+this.add.rectangle().setOrigin(0),
