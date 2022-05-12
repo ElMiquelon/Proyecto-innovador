b4c7afde84040a5876229553daa7b077817a247e
ElMiquelon <miguel.rhelguera@alumnos.udg.mx>
2022-05-12 17:40:06 -0500

    Actualización: mapa
    Ahora ya no es algo de lo que avergonzarse
    -actualizado el dialogo de eki al inicio diciendo que estaba en el taller de mecanica

--- scr/escenas/Funciones/verMapa.js
+++ scr/escenas/Funciones/verMapa.js
@@ -4,28 +4,8 @@ export default class verMapa extends Phaser.Scene{
     }
 
     create(){
-        this.cameras.main.setBounds(0,0,2000,2000);
-        this.cameras.main.setZoom(.25);
-        //this.add.text(0,0,'Si ves esto, cierra el mapa y abrelo de nuevo\nNo se porqué sucede :(',{fontSize: '200px',color: '#fff'}).setVisible(false)
-        var mapa = this.add.image(1000,1000, 'polimapa').setScale(0);
+        var mapa = this.add.image(250,250, 'minipolimapa').setScale(0);
         //esta madre se puede simplificar si simplemente diseñamos una imagen
-        var pasillos = this.add.group([
-            //pasillos del A
-            this.add.rectangle(536, 927, 55, 104, 0x00ffff).setOrigin(0,0),
-            this.add.rectangle(591, 927, 521, 28, 0x00ffff).setOrigin(0,0),
-            this.add.rectangle(591, 1003, 521, 28, 0x00ffff).setOrigin(0,0),
-            //Pasillos del B
-            this.add.rectangle(537,638,45,119,0x00ffff).setOrigin(0,0),
-            this.add.rectangle(583,682,529,24,0x00ffff).setOrigin(0,0),
-            //Pasillo del C
-            this.add.rectangle(785,560, 339, 27, 0x00ffff).setOrigin(0,0),
-            //Pasillos del D
-            this.add.rectangle(630,307,27,94,0x00ffff).setOrigin(0,0),
-            this.add.rectangle(658,307,458,28,0x00ffff).setOrigin(0,0),
-            //Pasillos del E
-            this.add.rectangle(566,189,551,34,0x00ffff).setOrigin(0,0),
-        ],{visible: false});
-
         this.events.on('transitionstart', (fromScene, duration)=>{
             this.tweens.add({
                 targets: mapa,
@@ -35,12 +15,7 @@ export default class verMapa extends Phaser.Scene{
             });
         });
 
-        this.events.on('transitioncomplete', ()=>{
-            pasillos.setVisible(true);
-        });
-
         this.events.on('transitionout', (toScene, duration)=>{
-            pasillos.setVisible(false);
             this.tweens.add({
                 targets:mapa,
                 scaleX:0,
